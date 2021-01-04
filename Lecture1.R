4870e24b89b489722ad2aeded53dac5493543998
Akihi-g <akihirokun@gmail.com>
2021-01-04 20:50:07 +0900

    Added discrimination and calibration

--- Lecture1.R
+++ Lecture1.R
@@ -1,17 +1,21 @@
 #Clinical prediction model
 library(tidyverse)
+library(DescTools) #Winsorize(x)
 library(gt)
 library(mice)
 library(rms)
 library(mfp)
 library(mgcv)
-library(foreign)
+library(VIM)
+library(naniar)
 library(ROCR)
 library(tableone)
 library(ICC)
 library(lmerTest)
+library(glmnet)
+library(Hmisc)
+library(ROCR)
 library(optimx)
-library(devtools)
 pkgFile <- "norm2_2.0.3.tar.gz"
 download.file(url = url, destfile = pkgFile)
 install.packages(pkgs=pkgFile, type="source", repos=NULL)
@@ -39,7 +43,17 @@ dat <- read_csv("C:/Users/akihi/Downloads/Clinical_prediction_model_lecture/CPR-
                 guess_max = 1500, 
                 na = "NA") 
 dat %>% glimpse()
-md.pattern(dat)
+par(mfrow=c(1,1))
+na.patterns <- naclus(dat)
+plot(na.patterns, ylab="Fraction of NAs in common")
+par(mfrow=c(2,2))
+naplot(na.patterns)
+na.pattern(dat)
+aggr(dat)
+ggplot(dat,
+       aes(x = hospital,
+           y = sbp)) +
+  geom_miss_point()
 summary(dat)
 dat_e <- dat %>% 
   mutate(age_cat = ifelse((gender=="TRUE" & age>70) | (gender=="FALSE" & age >= 75), 1, 0)) %>% 
@@ -57,9 +71,141 @@ summary(dat)
 #Complete case analysis
 ##Development
 full <- lrm(death ~ age+gender+hr+rr, data=dat_c, x = TRUE, y = TRUE)
+full #Check c-statistics
+full_int <- lrm(death ~ age*(gender+hr+rr), data=dat_c, x = TRUE, y = TRUE)
+full_up <- lrm(death ~ age+gender+hr+rr+sbp, data=dat_c, x = TRUE, y = TRUE)
+#update(full, .~. + age*gender + age*hr + age*rr)
+#Breier score
+B <- mean((full$y) * (1-plogis(full$linear.predictors))^2 + 
+                (1-full$y) * plogis(full$linear.predictors)^2)
+B
+Bmax <- mean(full$y) * (1-mean(full$y))
+Bmax
+Bscaled <- 1-B/Bmax
+Bscaled
+#Pearson R2
+cor(x=plogis(full$linear.predictors), y=full$y)^2
+#Calibration
 val.prob(logit=full$linear.predictor, y=full$y)
+#Set up Hosmer-Lemeshow test
+#p:predicted probability
+#Y:outcome variable
+#g:number of groups to calculate H-L (10 is default)
+hl.ext2<-function(p,y,g=10, df=g-1)
+{
+  matres	<-matrix(NA,nrow=g,ncol=5)
+  sor	<-order(p)
+  p	<-p[sor]
+  y	<-y[sor]
+  groep	<-cut2(p,g=g)									#g more or less equal sized groups
+  
+  len		<-tapply(y,groep,length)					#n per group
+  sump	<-tapply(p,groep,sum)						#expected per group
+  sumy	<-tapply(y,groep,sum)						#observed per group
+  meanp	<-tapply(p,groep,mean)						#mean probability per group
+  meany	<-tapply(y,groep,mean)						#mean observed per group
+  matres	<-cbind(len,meanp,meany, sump, sumy)
+  contr<-((sumy-sump)^2)/(len*meanp*(1-meanp))		#contribution per group to chi square
+  chisqr<-sum(contr)									#chi square total
+  pval<-1-pchisq(chisqr,df)							#p-value corresponding to chi square with df degrees of freedom 
+  cat("\nChi-square",chisqr," p-value", pval,"\n")
+  dimnames(matres)	<-list(c(1:g),Cs(n,avg(p),avg(y), Nexp, Nobs))
+  result  <- list(table(groep), matres,chisqr,pval) 
+}
+hl.ext2(p=plogis(full$linear.predictor),y=full$y, g=10, df=9) #Df: external -> g-1, internal -> g-2
+#Disrimination
+#Discrimination slope
+mean(plogis(lp[full$y==1])) - mean(plogis(lp[full$y==0]))
+#ROC curve
+#Lorenz curve
+pred.full <- prediction(full$linear.predictor, full$y)
+perf <- performance(pred.full, "tpr", "fpr")
+plot(perf)
+auc.tmp <- performance(pred.full,"auc")
+auc <- as.numeric(auc.tmp@y.values)
+auc
+perf <- performance(pred.full, "sens", "spec")
+plot(perf)
+perf.full <- performance(pred.full,"fnr","rnp") # Lorenz curve data+plot
+plot(perf.full, xlab="Fraction not resected", ylab="Tumors missed")
+abline(a=0,b=1)
+#Box plot
+boxplot(plogis(full$linear.predictors)~full$y,
+        ylab="Predicted risk", xlab="Death",ylim=c(0,1))
+#Scatter plot
+plot(x= plogis(full$linear.predictors), y= plogis(full_up$linear.predictors),
+     xlab="Predicted risk without SBP", ylab="Predicted risk with SBP",
+     las=1,pty='s',xlim=c(0,1),ylim=c(0,1),
+     pch=(full$y+.5)*2) # pch 1 and 3
+abline(a=0,b=1)
+#CI around c-statistics
+cstatNo <- rcorr.cens(full$linear.predictors, full$y) 
+cat(cstatNo[1], "[", cstatNo[1]-1.96/2*cstatNo[3], " - ", cstatNo[1]+1.96/2*cstatNo[3],"]")
+cstatYes <- rcorr.cens(full_up$linear.predictors, full$y) 
+cat(cstatYes[1], "[", cstatYes[1]-1.96/2*cstatYes[3], " - ", cstatYes[1]+1.96/2*cstatYes[3],"]")
+cstatYes - cstatNo
+cstatvAL <- rcorr.cens(lp.val, val$death) 
+cat(cstatvAL[1], "[", cstatvAL[1]-1.96/2*cstatvAL[3], " - ", cstatvAL[1]+1.96/2*cstatvAL[3],"]")
+par(mfrow = c(1,3), pty='s', mar=c(4.2,4,4,1),cex=0.95, font =1, col=1)
+val.prob.ci(logit=full$linear.predictor,y=full$y, pl=T,smooth=T,logistic.cal=F, g=10,
+            xlab="Predicted risk",
+            ylab="Observed death",riskdist='predicted',
+            d1lab="Death", d0lab="Alive", dist.label=-0.95, cutoff=.2)
+#NRI&IDI
+slopeNo <- mean(plogis(full$linear.predictors[full$y==1])) - mean(plogis(full$linear.predictors[full$y==0]))
+slopeYes <- mean(plogis(full_up$linear.predictors[full_up$y==1])) - mean(plogis(full_up$linear.predictors[full_up$y==0]))
+slopeYes - slopeNo #IDI
+NoSBP <- cut(plogis(full$linear.predictors),breaks = c(0,.2,1), labels = c("No SBP, <=20%","No SBP, >20%"))
+YesSBP <- cut(plogis(full_up$linear.predictors),breaks = c(0,.2,1), labels = c("With SBP, <=20%","With SBP, >20%"))
+ValReclass <- cut(plogis(lp.val),breaks = c(0,.2,1), labels = c("Val, <=20%","Val, >20%"))
+table(NoSBP)
+table(YesSBP)
+tabReclas <- table(NoSBP, YesSBP)
+tabReclas
+tabReclas[2]/(tabReclas[2]+tabReclas[4]) # reclassified among high risk
+tabReclas[3]/(tabReclas[1]+tabReclas[3]) # reclassified among low risk
+table(NoSBP, YesSBP, full$y) #By outcome
+tab <- table(NoSBP, YesSBP, full$y )
+tab[5:8]/(tab[1:4]+tab[5:8])
+table(NoSBP, full$y )
+tabNo <- table(NoSBP, full$y )
+tabNo[3:4]+tabNo[1:2]
+tabNo[3:4]/(tabNo[3:4]+tabNo[1:2])
+table(YesSBP, full$y )
+tabYes <- table(YesSBP, full$y )
+tabYes[3:4]+tabYes[1:2]
+tabYes[3:4]/(tabYes[3:4]+tabYes[1:2])
+improveProb(x1=as.numeric(NoSBP)-1, x2=as.numeric(YesSBP)-1, y=full$y) #NRI and IDI calculations with Harrell's functions
 ##Internal validation
-validate(full, B=200) 
+validate(full, B=200)
+B <- 200
+for (i in 1:B) {
+  dat_c_b <- dat_c[sample(nrow(dat_c), replace=T),]
+  full.gam.gcv.B <- gam(death ~ s(age) + gender+s(hr)+s(rr), data = dat_c, family = binomial)
+  val.prob(y=dat_c_b$death, logit=predict(full.gam.gcv.B))
+}
+#Bootstrap discrimination slope
+nrowB	<- nrow(dat_c)
+B <- 200            # 200 bootstraps
+matB <- matrix(NA,nrow=B,ncol=3) # Matrix for results
+dimnames(matB) <- list(c(1:B), Cs(Slopeapp, Slopetest, optimism ))
+for (i in 1:B) {
+  if (i%%10==0) cat("Start Bootstrap sample nr", i, "\n")
+  Brows <- sample(nrowB,replace=T)
+  # Bsample is bootstrap sample from development set
+  Bsample	<- dat_c[Brows,]
+  devfull <- lrm(death ~ age+gender+rr+hr, data=Bsample,linear.predictors=T, x=T, y=T)
+  matB[i,1] <- mean(plogis(devfull$linear.predictors[devfull$y==1])) - 
+    mean(plogis(devfull$linear.predictors[devfull$y==0]))
+  lp  <- full$x %*% devfull$coef[2:length(devfull$coef)] + devfull$coef[1] # lp with coefs from bootstrap
+  matB[i,2] <- mean(plogis(lp[full$y==1])) - mean(plogis(lp[full$y==0]))  # Testing on original sample
+  
+}
+cat("\n\nEinde bereikt!\n\n")
+matB[,3] <- matB[,1] - matB[,2] # optimism
+# matB bekijken mean en SD
+apply(matB,2,mean)
+apply(matB,2,sd)
 ##External validation
 dat_ec <-dat_ec %>% 
   mutate(score = age_cat+bun_cat+rr_cat+bp_cat+ams) %>% 
@@ -70,23 +216,17 @@ table1 <- CreateTableOne(vars = "death",
 summary(table1)
 ext.full <- lrm(death ~ age_cat+bun_cat+rr_cat+bp_cat+ams,
                 data=dat_ec, x=TRUE, y=TRUE)
-###Restricted cubic splines
+###Penalization
+#Restricted cubic splines
 lrm(death ~ rcs(age,4) + gender + rcs(hr,4)+rcs(rr,4), data = dat_c)
-###Fractional polynomials
-mfp(death ~ fp(age) + gender + fp(hr)+fp(rr), alpha = 0.2, select = 0.2, data = dat_c, family = binomial)
-###Generalized additive model
+#Fractional polynomials
+mfp(death ~ fp(age) + gender + fp(hr)+fp(rr), alpha = 1, data = dat_c, family = binomial)
+#Generalized additive model
 gam(death ~ s(age) + gender+s(hr)+s(rr), data = dat_c, family = binomial)
-###LASSO
-glmmod <- glmnet(full8$x, y=full8$y, alpha=1, family="binomial")
-###Internal validation by bootstrap
-B <- 5000
-for (i in 1:B) {
-  dat_c_b <- dat_c[sample(nrow(dat_c), replace=T),]
-  full.gam.gcv.B <- gam(death ~ s(age) + gender+s(hr)+s(rr), data = dat_c, family = binomial)
-  val.prob(y=dat_c_b$death, logit=predict(full.gam.gcv.B))
-}
-###Multilevel model
-###Random intercept model
+#LASSO
+glmmod <- glmnet(full$x, y=full$y, alpha=1, family="binomial")
+##Multilevel model
+#Random intercept model
 ICCest(as.factor(hospital),death, data=dat_c, alpha=0.05, CI.type="Smith")
 dat_c$age.m <- ave(dat_c$age, dat_c$hospital)
 dat_c$age.cwc <- dat_c$age - dat_c$age.m
@@ -109,11 +249,12 @@ dat_mi0$method
 dat_mi0$predictorMatrix
 dat_mi10 <- mice(dat, m = 10, maxit = 20, printFlag = FALSE, seed = 1234) 
 plot(dat_mi10)
+densityplot(dat_mi0)
 mira <- with(dat_mi0, glm(death ~ age + gender + rr + hr,  family = binomial))
 result <- summary(pool(mira))
 exp(result[2,2])
 exp(result[2,3])
-###Another way
+#Another way
 est <- se <- vector(length = dat_mi10$m, mode = "list")
 for (i in 1:dat_mi10$m) {
   com <- complete(dat_mi10, i)
@@ -126,8 +267,8 @@ miinf <- miInference(est, se)
 print(miinf)
 exp(miinf$est)
 exp(miinf$std.err)
-###Multilevel model
-###Random intercept model
+##Multilevel model with MI dataset
+#Random intercept model
 est <- se <- vector(length = dat_mi10$m, mode = "list")
 for (i in 1:dat_mi10$m) {
   com <- complete(dat_mi10, i)
@@ -151,3 +292,4 @@ miinf <- miInference(est, se)
 print(miinf)
 exp(miinf$est)
 exp(miinf$std.err)
+##Try to creat MI data sets and apply bootstrapping to each of them
